ca79bcf3673716c109e160b83abdc5d07a176b7d
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-04-19 22:07:25 +0300

    fandoms, my fandoms and recommendation created

---
+++ lib/view/fandoms.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FandomsPage extends StatefulWidget {
+  @override
+  _FandomsPageState createState() => _FandomsPageState();
+}
+
+class _FandomsPageState extends State<FandomsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fandoms"),
+      ),
+      body: Center(
+        child: Text("On Progress..."),
+      ),
+    );
+  }
+}
---
+++ lib/view/my_fandoms.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyFandomsPage extends StatefulWidget {
+  @override
+  _MyFandomsPageState createState() => _MyFandomsPageState();
+}
+
+class _MyFandomsPageState extends State<MyFandomsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fandoms"),
+      ),
+      body: Center(
+        child: Text("On Progress..."),
+      ),
+    );
+  }
+}

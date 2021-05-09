a041bc2d34bebb4a66fc99e2d6cff8848c8fad32
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-05-09 18:47:36 +0300

    Fandom page updated

---
+++ lib/util/components/fandoms_list_tile.dart
@@ -0,0 +1,6 @@
+import 'package:fandom_app/models/fandom.dart';
+import 'package:flutter/cupertino.dart';
+
+Container fandomListTile({@required BuildContext context, @required Fandom fandom}) {
+
+}
--- lib/view/fandoms.dart
+++ lib/view/fandoms.dart
@@ -1,3 +1,4 @@
+import 'package:fandom_app/util/components/fandoms_list_tile.dart';
 import 'package:fandom_app/util/constants/app_state_enum.dart';
 import 'package:fandom_app/view_models/fandom_view_model.dart';
 import 'package:flutter/material.dart';
@@ -17,8 +18,31 @@ class _FandomsPageState extends State<FandomsPage> {
         appBar: AppBar(
           title: Text("Fandoms"),
         ),
-        body: Center(
-          child: Text("On Progress...${_fandomVM.allFandoms.length}"),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _fandomVM.allFandoms.isEmpty
+                    ? CircularProgressIndicator()
+                    : ListView.separated(
+                    primary: false,
+                    itemBuilder: (context, index) {
+                      print("${_fandomVM.allFandoms[index].fid}");
+                      return fandomListTile(
+                          context: context, fandom: _fandomVM.allFandoms[index]);
+                    },
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        height: 18.0,
+                      );
+                    },
+                    itemCount: _fandomVM.allFandoms.length),
+              ],
+            ),
+          ),
         ),
       );
     } else {
